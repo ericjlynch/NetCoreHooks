54b1b10427170cf212ac810a1d545f52549e03a6
Eric Lynch <eric@ericjlynch.com>
2020-08-31 14:03:58 -0400

    Stubs for Inline Hook response

---
+++ NetCoreHooks/Controllers/RegistrationController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NetCoreHooks.Contracts;
+using NetCoreHooks.DTOs;
+using Newtonsoft.Json.Linq;
+
+namespace NetCoreHooks.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+        private readonly IEmployeeRepository _db;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+
+
+        public RegistrationController(ILoggerService loggerService,
+            IEmployeeRepository employeeRepository, IMapper mapper, IConfiguration configuration)
+        {
+            _logger = loggerService;
+            _db = employeeRepository;
+            _mapper = mapper;
+            _config = configuration;
+        }
+
+        [HttpPost("dbLookup")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> VerifyEmployeeSSNByUserName()
+        {
+            string ssnFromOkta = String.Empty;
+            string userName = String.Empty;
+            string ssnFromDatabase = String.Empty;
+
+            //grab http reques body
+            var reader = new StreamReader(Request.Body);
+            var payLoad = await reader.ReadToEndAsync();
+            Debug.WriteLine(payLoad);
+
+            //convert incoming http request to JSon JObject
+            var parsedJson = JObject.Parse(payLoad);
+
+            //make sure you have a good payload
+            if(payLoad != null)
+            {
+                //get SSN from payload via Linq-to-JSon
+                var userProfile = (JObject)parsedJson.SelectToken("data")
+                    .SelectToken("userProfile");
+                userName = userProfile["login"].ToString();
+
+                /*did you remember to include SSN in Okta User Profile? 
+                 * if you didn't, the ssn key will be null.
+                */
+                if(userProfile["ssn"] != null)
+                {
+                    ssnFromOkta = userProfile["ssn"].ToString();
+                    Debug.WriteLine(ssnFromOkta);
+                } else
+                {
+                    return BadRequest("Incoming SSN was null");
+                }
+
+                //get ssn from database for this user
+                var employee = await _db.FindByUserName(userName);
+                var response = _mapper.Map<EmployeeDTO>(employee);
+                ssnFromDatabase = response.SSN;
+                Debug.WriteLine(ssnFromDatabase);
+
+                //do the SSNs match? 
+                if(ssnFromOkta == ssnFromDatabase)
+                {
+                    //you have a match
+                    Debug.WriteLine("SSN match detected. Returing 200 OK");
+                    return Ok("User profiles match");
+                }
+                else
+                {
+                    //no match. Disallow registration
+                    return Unauthorized("Customer SSNs do not match");
+                }
+
+            } else
+            {
+                //payLoad was null
+                return BadRequest("no incoming payload detected");
+            }           
+        }
+    }
+}
+ 
